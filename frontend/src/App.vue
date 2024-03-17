8b44430697eca5af4f646a37919a69ed54535ad4
Willian <willianisami1229@gmail.com>
2024-03-16 23:59:11 -0300

    Create Vue.js setup for the frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Roulette image" src="./assets/roulette_wheel.jpg">
+  <Roulette msg="Welcome to Our Game"/>
+</template>
+
+<script>
+import Roulette from './components/Welcome.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Roulette
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+img {
+  width: 400px;
+}
+</style>./components/Welcome.vue
---
+++ frontend/src/components/Welcome.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="roulette">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to play<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">rules</a>.
+    </p>
+    <h2>Have some fun <a href="">Play</a></h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RouletteComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
